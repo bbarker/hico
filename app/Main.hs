d90f54691c64b7b59344c31be9fb80b8ab12920a
Brandon Elam Barker <beb82@cornell.edu>
2018-09-15 15:55:37 +0000

    refactored

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-{-# LANGUAGE DuplicateRecordFields #-}
-
-
 module Main where
 
-import           Data.Semigroup ((<>))
 import           Hico
 import           Options.Applicative
-import           SDL            (RendererConfig, RendererType(SoftwareRenderer), defaultRenderer, rendererType)
+
 
 data SomeEnv = SomeEnv {
   x :: Int
@@ -35,59 +31,6 @@ exampleGame cfg = Game {
   draw = draw'
 }
 
-data RawRunConfig = RawRunConfig {
-  widthIn  :: Int,
-  heightIn :: Int,
-  renderer :: Maybe RendererType
-}
-
-processRunConfig :: RawRunConfig -> GameConfig
-processRunConfig raw = GameConfig{
-   width    = widthIn(raw)
-   ,height  = heightIn(raw)
-  ,renderer = maybe defaultRendererType id (rendererRaw(raw))
-  }
-  where
-    rendererRaw(raw) = renderer(raw:: RawRunConfig)
-
-
-xresP :: Parser Int
-xresP = option auto(
-  long "horizontal-res"
-  <> short 'x'
-  <> help "Width; Horizontal (x) axis resolution for game"
-  <> showDefault
-  <> value 640
-  <> metavar "INT"
-  )
-
-yresP :: Parser Int
-yresP = option auto
-          ( long "vertical-res"
-         <> short 'y'
-         <> help "Height; Vertical (y) axis resolution for game"
-         <> showDefault
-         <> value 480
-         <> metavar "INT")
-
-defaultRendererType :: RendererType
-defaultRendererType = rendererType(defaultRenderer)
-
-sdlDefaultRendererP :: Parser RendererType
-sdlDefaultRendererP = flag' defaultRendererType (
-  long "default_rndr"
-  <> help "Use SDL's default renderer")
-
-sdlsoftwareRendererP :: Parser RendererType
-sdlsoftwareRendererP = flag' SoftwareRenderer (
-  long "software_rndr"
-  <> help "Use SDL's default renderer")
-
-rendererP :: Parser (Maybe RendererType)
-rendererP = optional (sdlDefaultRendererP <|> sdlsoftwareRendererP)
-
-parseCliConfig :: Parser RawRunConfig
-parseCliConfig = RawRunConfig <$> xresP <*> yresP <*> rendererP
 
 main :: IO ()
 main = doConfig =<< execParser opts
@@ -99,6 +42,6 @@ main = doConfig =<< execParser opts
       )
 
 
-doConfig :: RawRunConfig -> IO()
+doConfig :: CliConfig -> IO()
 doConfig runConf =
   runHicoGame (exampleGame(processRunConfig(runConf)))
